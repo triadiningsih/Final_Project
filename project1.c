eb14d9fdc6df2e7ac399b06d929fef7ac12e44cc
Teguh Permana <teguhpermana096@gmail.com>
2021-12-15 10:24:19 +0800

    project3_teguh

--- project1.c
+++ project1.c
@@ -1,7 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
+struct data_telepon{
+    int no_telepon;
+    char nama[50];
+    char alamat[50];
+    char email[50];
+    char note[50];
+    int indexing;
+}
+struct data_telepon hash_table[100];
+int domain_hashing(int n);
+void init(int n);
 int main(){
-    for(int i=0;i<10;i++)
-        printf("Test");
+    int n;
+    printf("Jumlah Domain Kontak :");scanf("%d",&n);
+    init(n);
+
+    domain_hashing(n);
     return 0;
+}
+void init(int n){
+    for(int i=0;i<n;i++){
+        hash_table[i]=NULL;
+    }
+}
+int domain_hashing(int n){
+    for(int i=0;i<n;i++){
+        
+    }
 }
